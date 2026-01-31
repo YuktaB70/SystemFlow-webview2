22e84cb1545f10217b30b29233f19bbd8e9ce4d6
YuktaB70 <163778093+YuktaB70@users.noreply.github.com>
2026-01-31 01:04:50 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Core;
+using System.Windows.Shapes;
+using System.IO;
+namespace SystemFlow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitWebView();
+
+        }
+
+        private async void InitWebView()
+        {
+            await webView.EnsureCoreWebView2Async(null); //initialize webView2
+
+
+            //Expose C# object to javascript file
+            webView.CoreWebView2.AddHostObjectToScript(
+                "system",
+                new SystemInfoComponents()
+                );
+
+            //Build absolute path to ui folder in the output directory
+            var uiPath = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "ui");
+            //var uiPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ui");
+            //var indexPath = Path.Combine(uiPath, "index.html");
+
+            webView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "app.local",
+                uiPath,
+                Microsoft.Web.WebView2.Core.CoreWebView2HostResourceAccessKind.Allow
+                );
+
+
+            webView.CoreWebView2.Navigate("https://app.local/index.html");
+            //webView.CoreWebView2.OpenDevToolsWindow();
+
+
+
+
+        }
+
+        private async void LoadTestPage()
+        {
+            await webView.EnsureCoreWebView2Async();
+
+            webView.CoreWebView2.Navigate("https://www.google.com/");
+        }
+
+    }
+}
---
+++ SystemInfoComponents.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Graph;
+
+[ComVisible(true)]
+public class SystemInfoComponents
+{
+
+    public string GetStorage()
+    {
+        DriveInfo drive = new DriveInfo(Path.GetPathRoot(
+            Environment.GetFolderPath(Environment.SpecialFolder.System)));
+
+        long total = drive.TotalSize;
+        long used = total - drive.AvailableFreeSpace;
+
+        double total_gb = (total / (1024.0 * 1024.0) / 1024.0);
+
+        double used_gb = (used / (1024.0 * 1024.0) / 1024.0);
+
+        double[] storageFolders = GetStorageInfo();
+        var obj =  new StorageInfo
+        {
+            TotalBytes = total_gb,
+            UsedBytes = used_gb,
+            Desktop = storageFolders[0],
+            Documents = storageFolders[1],
+            Downloads = storageFolders[2],
+            Pictures = storageFolders[3],
+            Videos = storageFolders[4],
+
+        };
+        return System.Text.Json.JsonSerializer.Serialize(obj);
+    }
+    public double[] GetStorageInfo()
+    {
+        double[] storage;
+        string Desktop_path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string Documents_path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        string downloads_path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+        string pictures_path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        string videos_path = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+
+
+        double Desktop = GetFolderSize(Desktop_path);
+        double Documents = GetFolderSize(Documents_path);
+        double Downloads = GetFolderSize(downloads_path);
+        double Pictures = GetFolderSize(pictures_path);
+        double Videos = GetFolderSize(videos_path);
+        storage = [Desktop, Documents, Downloads, Pictures, Videos];
+        return storage;
+
+    }
+
+    public double GetFolderSize(string path)
+    {
+        long size = 0;
+        foreach (var file in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories))
+        {
+            try { size += new FileInfo(file).Length; }
+            catch { }
+        }
+
+        double mb = size / (1024.0 * 1024.0);
+        double gb = (mb / 1024.0);
+
+        return gb;
+    }
+}
+
+
+[ComVisible(true)]
+public class StorageInfo
+{
+    public double TotalBytes { get; set; }
+    public double UsedBytes { get; set; }
+    public double Desktop { get; set; }
+    public double Documents { get; set; }
+    public double Downloads { get; set; }
+    public double Pictures { get; set; }
+    public double Videos { get; set; }
+
+}
